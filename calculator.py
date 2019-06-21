e4744312b88cd280599163ca627dd582e6a3ac89
sinclairr <rickysinclair@gmail.com>
2019-06-21 11:07:34 +1000

    21/06/2019 v0.1

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# prompt user for input
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+oper = input("Enter the operator: ")
+
+# perform calculations
+if oper == "*":
+    ans = num1 * num2
+elif oper == "-":
+    ans = num1 - num2
+elif oper == "+":
+    ans = num1 + num2
+elif oper == "/":
+    ans = num1 / num2
+else:
+    print("You have entered an invalid operator")
+    exit()
+
+print(num1, oper, num2, ans)
